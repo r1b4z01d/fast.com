a5e2b1844ccd8457bb5154c01da9d254e375bb16
r1b4z01d <bryanribas@gmail.com>
2018-02-06 10:58:48 -0600

    Added Logging

--- fast_com_example_usage.py
+++ fast_com_example_usage.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
 import fast_com
+import schedule
+import time
+import datetime
 
-print "Start speedtest against fast.com ..."
-print "Result:", fast_com.fast_com(), "Mbps"
-print "... Done"
+def job():
+	speed = fast_com.fast_com()
+	print "Result:", speed, "Mbps"
+	st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+	with open("speeds.log", "a") as logFile:
+		logFile.write(st +","+str(speed))
+		logFile.close()
 
+schedule.every().minute.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
